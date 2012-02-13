74777f4af698c972be57faa37e73fd6d07e75f39
Anantha Kumaran <ananthakumaran@gmail.com>
2012-02-13 12:32:20 +0530

    adds validate spec

--- lib/fake_dynamo/validation.rb
+++ lib/fake_dynamo/validation.rb
@@ -6,14 +6,22 @@ module FakeDynamo
     extend ActiveSupport::Concern
     include ActiveModel::Validations
 
+
     def validate!
       if invalid?
         raise ValidationException, errors.full_messages.join(', ')
       end
     end
 
+
+    def add_errors(message)
+      @api_errors ||= []
+      @api_errors << message
+    end
+
     def validate_payload(operation, data)
       validate_operation(operation)
+      validate_input(operation, data)
     end
 
     def validate_operation(operation)
@@ -22,6 +30,56 @@ module FakeDynamo
 
     def available_operations
       api_config[:operations].keys
+    end
+
+    def validate_input(operation, data)
+      api_input_spec(operation).each do |attribute, spec|
+        validate_spec(attribute, data[attribute], spec, [])
+      end
+    end
+
+    def validate_spec(attribute, data, spec, parents)
+      if spec.include?(:required) and not data
+        add_errors("value null at '#{param(attribute, parents)}' failed to satisfy the constraint: Member must not be null")
+        return
+      end
+
+      spec.each do |constrain|
+        case constrain
+        when :string
+          add_errors("The parameter '#{param(attribute, parents)}' must be a string") unless data.kind_of? String
+        when :long
+          add_errors("The parameter '#{param(attribute, parents)}' must be a long") unless data.kind_of? Fixnum
+        when Hash
+          case constrain.keys.first
+          when :pattern
+            pattern = constrain[:pattern]
+            unless data =~ pattern
+              add_errors("The parameter '#{param(attribute, parents)}' should match the pattern #{pattern}")
+            end
+          when :structure
+            structure = constrain[:structure]
+            new_parents = parents + [attribute]
+            structure.each do |attribute, spec|
+              validate_spec(attribute, data[attribute], spec, new_parents)
+            end
+          else
+            raise "Unhandled constraint #{constrain}"
+          end
+        when :required
+          # handled earlier
+        else
+          raise "Unhandled constraint #{constrain}"
+        end
+      end
+    end
+
+    def param(attribute, parents)
+      (parents + [attribute]).join('.')
+    end
+
+    def api_input_spec(operation)
+      api_config[:operations][operation][:input]
     end
 
     def api_config
